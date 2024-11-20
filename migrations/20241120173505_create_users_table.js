7adab6d229315c01c379056c91914e90870cabd5
Zuri Gomez <zgo522@gmail.com>
2024-11-20 12:49:39 -0500

    Create migration folder and files, add table content for users and listings

---
+++ migrations/20241120173505_create_users_table.js
@@ -0,0 +1,27 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export function up(knex) {
+    return knex.schema
+
+    .createTable("user", (table) => {
+        table.increments("id").primary();
+        table.string("user_name").notNullable();
+        table.string("email").notNullable();
+        table.string("password").notNullable();
+        table.string("confirm password").notNullable();
+        table.timestamp("created_at").defaultTo(knex.fn.now());
+        table
+          .timestamp("updated_at")
+          .defaultTo(knex.raw("CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP"));
+      })
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export function down(knex) {
+    return knex.schema.dropTable('users');
+};
---
+++ migrations/20241120173543_create_listings_table.js
@@ -0,0 +1,46 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export function up(knex) {
+  return knex.schema.createTable("user", (table) => {
+    table.increments("id").primary();
+    table.integer("user_id").unsigned().notNullable();
+    table.string("photo").notNullable();
+    table.string("title").notNullable();
+    table.text("description").notNullable();
+    table.enu("maintenance", ["low", "medium", "high"]).notNullable();
+    table.enu("pot_included", ["yes", "no"]).notNullable();
+    table.string("pot_description");
+    table.decimal("height", 5, 2).notNullable();
+    table
+      .enu("sunlight", [
+        "full sun",
+        "partial sun",
+        "partial shade",
+        "full shade",
+      ])
+      .notNullable();
+    table.string("temperature").notNullable();
+    table
+      .enu("watering", ["once per week", "every two weeks", "every month"])
+      .notNullable();
+    table.decimal("price", 10, 2).notNullable();
+    table.enu("delivery", ["pickup", "delivery"]).notNullable();
+    table.timestamp("created_at").defaultTo(knex.fn.now());
+
+    table
+      .foreign("user_id")
+      .references("id")
+      .inTable("users")
+      .onDelete("CASCADE");
+  });
+}
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export function down(knex) {
+  return knex.schema.dropTable("listings");
+}
